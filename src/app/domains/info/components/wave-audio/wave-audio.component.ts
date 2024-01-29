3e7e7d1929eb9ae3d237702161ea0c9797dd59ce
brendasutara <sutarabrenda@gmail.com>
2024-01-29 09:39:01 -0300

    Cambios en página About

---
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Input, ViewChild, signal } from '@angular/core';
+import WaveSurfer from 'wavesurfer.js'
+
+@Component({
+  selector: 'app-wave-audio',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './wave-audio.component.html',
+  styleUrl: './wave-audio.component.css'
+})
+export class WaveAudioComponent {
+
+  @Input({required: true}) audioUrl!: string;
+  @ViewChild('wave') container!: ElementRef;
+  private ws!: WaveSurfer;
+  isPlaying = signal(false);
+
+  ngAfterViewInit() {
+    this.ws = WaveSurfer.create({
+      url: this.audioUrl,
+      container: this.container.nativeElement
+    });
+    this.ws.on('play', () => this.isPlaying.set(true));
+    this.ws.on('pause', () => this.isPlaying.set(false));
+  }
+
+  playPause() {
+    this.ws.playPause()
+  }
+
+}
--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
 import { CounterComponent } from '../../../shared/components/counter/counter.component';
+import { WaveAudioComponent } from '../../components/wave-audio/wave-audio.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, CounterComponent],
+  imports: [CommonModule, CounterComponent, WaveAudioComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
