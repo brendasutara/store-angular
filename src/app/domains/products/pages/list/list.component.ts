e18b7ad8aeff29be31a648483c6ffc182af1285b
brendasutara <sutarabrenda@gmail.com>
2024-01-29 10:57:00 -0300

    Componente Product: enviando atributo por atributo

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -10,6 +11,27 @@ import { CommonModule } from '@angular/common';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23'
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 2',
+        price: 350,
+        image: 'https://picsum.photos/640/640?r=12'
+      }
+    ];
+    this.products.set(initProducts)
+  }
+
   fromChild (event: string) {
     console.log('estamos en el padre');
     console.log(event);
