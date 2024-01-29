e735c394afed0a48bb4968c943183850fd17941a
brendasutara <sutarabrenda@gmail.com>
2024-01-29 09:02:16 -0300

    Evitando memory leaks con ngDestroy

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, SimpleChange } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -11,6 +11,8 @@ import { Component, Input, SimpleChange } from '@angular/core';
 export class CounterComponent {
   @Input({required: true}) duration = 0;
   @Input({required: true}) message = '';
+  counter = signal(0);
+  counterRef: number | undefined;
 
   constructor() {
     //no async
@@ -20,11 +22,15 @@ export class CounterComponent {
     console.log('-'.repeat(10))
   }
 
-  ngOnChanges(changes: SimpleChange) {
+  ngOnChanges(changes: SimpleChanges) {
     //before and during render
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
-    console.log(changes)
+    console.log(changes);
+    const duration = changes['duration'];
+    if(duration && duration.currentValue !== duration.previousValue) {
+      this.doSomething();
+    }
   }
 
   ngOnInit() {
@@ -35,6 +41,11 @@ export class CounterComponent {
     console.log('-'.repeat(10));
     console.log('duration => ', this.duration);
     console.log('message => ', this.message);
+    //cuidado que con setInterval se sigue ejecutando aunque le haya hecho Destroy
+    this.counterRef = window.setInterval(() => {
+      console.log('run interval')
+      this.counter.update(statePrev => statePrev + 1)
+    }, 1000)
   }
 
   ngAfterViewInit() {
@@ -47,5 +58,10 @@ export class CounterComponent {
   ngOnDestroy() {
     console.log('ngOnDestroy');
     console.log('-'.repeat(10));
+    window.clearInterval(this.counterRef)
+  }
+
+  doSomething() {
+    console.log('change duration')
   }
 }
